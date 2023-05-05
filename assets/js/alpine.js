7d8b77c6cbe0abf5c870e04bae0926f1c1095920
Evaldas Romaškis <evaldasr@gmail.com>
2023-05-05 14:39:20 +0300

    add alpine collapse plugin

--- assets/js/alpine.js
+++ assets/js/alpine.js
@@ -1,5 +1,8 @@
 import Alpine from 'alpinejs'
+import AlpineCollapse from '@alpinejs/collapse'
 import './alpine/offcanvas.js'
+
+Alpine.plugin( AlpineCollapse )
 
 window.Alpine = Alpine
 Alpine.start()
